c1dda3cd32838bd2f60cf1b145f679222f266da3
Kristof Gilicze <kristof.gilicze@horizon-alpha.com>
2025-11-21 14:18:16 +0100

    experimenting

--- 03_pointertypes/pointertypes.c
+++ 03_pointertypes/pointertypes.c
@@ -15,21 +15,35 @@
 // Pointer + sizeof + unterschiedliche Typgrößen.
 
 int main() {
-    int mint = 3;
-    float mfloat = 3.0;
+    // int mint = 3;
+    // float mfloat = 3.0;
+    char mterm = '\0';
+    char mchar3 = 'c';
+    char mchar2 = 'b';
     char mchar = 'a';
-    printf("Int %d\n", mint);
-    printf("Float %f\n", mfloat);
-    printf("Char %c\n", mchar);
+
+    char* a = &mchar2;
+
+    printf("Als char: %c\n", mchar2);
+    printf("Als int:  %d\n", &mchar2);
+    printf("Als hex:  0x%x\n", mchar2);
+    printf("Adresse:  %p\n", &mchar2);
+    printf("Wert an Adresse: %d\n", *(&mchar2));
+    printf("Wert an Adresse: %d\n", a);
+    // printf("Int %d\n", mint);
+    // printf("Float %f\n", mfloat);
+    // printf("Char %c\n", mchar);
 
 
-    printf("Address Int %p\n", &mint);
-    printf("Address Float %p\n", &mfloat);
-    printf("Address Char %p\n", &mchar);
+    // printf("Address a %p\n", &mchar);
+    // printf("Address b %p\n", &mchar2);
+    // printf("Address c %p\n", &mchar3);
+    // printf("Address term %p\n", &mterm);
+    // printf("String test: %s\n", &mchar); // <--- sollte doch "abc" printen
 
-    printf("Size of Int %lu\n", sizeof(mint));
-    printf("Size of Float %lu\n", sizeof(mfloat));
-    printf("Size of Char %lu\n", sizeof(mchar));
+    // printf("Size of Int %lu\n", sizeof(mint));
+    // printf("Size of Float %lu\n", sizeof(mfloat));
+    // printf("Size of Char %lu\n", sizeof(mchar));
 
     return 0;
 }
